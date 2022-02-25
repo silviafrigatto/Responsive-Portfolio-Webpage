bb6562b6d3d5520592acfa994a5fb4e54dc3cd46
Silvia Frigatto <silvia.frigatto@gmail.com>
2022-02-25 18:11:26 -0300

    Finished menu itens positioning

--- src/script.js
+++ src/script.js
@@ -1,8 +1,22 @@
-const hamburgerButton = document.getElementById('hamburger')
+/*const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 
 function toggleButton() {
     navList.classList.toggle('show')
 }
 
-hamburgerButton.addEventListener('click', toggleButton)
+hamburgerButton.addEventListener('click', toggleButton)*/
+
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = () => {
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+}
+
+cancelBtn.onclick = () => {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+}
